89536b86aa278f4f0302d69d858de6b9a05899a6
Maartenapd0 <Maarteno55xi@gmail.com>
2021-03-19 17:00:23 +0100

    opdracht p3l6

--- p3l6.py
+++ p3l6.py
@@ -1,23 +1,28 @@
+#delen importeren 
+
 from tkinter import *
 import time as t
 import random as r
 import turtle
 import random
+
+# voribale de regen boog knop
+
 colors = ["red", "green", "blue"]
 colorss = ["orange", "black", "purple"]
 color = random.choice(colors)
-
+# venster aanmaken
 window = Tk()
 
 window.title("Welcome to LikeGeeks app")
 window.geometry('350x200')
-
+#stukje tekst toevoegen
 lbl = Label(window, text="your name")
 lbl.grid(column=0, row=0)
-
+#tekst kunnen toevoegen
 txt = Entry(window,width=10)
 txt.grid(column=1, row=2)
-
+#de hele mooie regen boog knop
 def regenboog():
     turtle.speed (10)
     turtle.bgcolor ("grey")
@@ -49,13 +54,14 @@ def regenboog():
         turtle.right(2)
         turtle.pendown()
         turtle.color(random.choice(colorss))
+#de submit knop voor je naam
 def clicked():
     res = "heey " + txt.get() + " hoe gaat het met jou"
     lbl.configure(text= res)
-
+#de actie voor de knoppen
 btn2 = Button(window, text="rainbow", command=regenboog)
 btn2.grid(column=0, row=2)
 btn1 = Button(window, text="Click Me", command=clicked)
 btn1.grid(column=2, row=0)
-
+# de hooft loop waar door alles blijft uitvoeren
 window.mainloop()
